34a904612adb106266f2b25269506986647b41f2
Youssef <Youssef.bawakny@gmail.com>
2015-09-17 18:50:04 +0300

    version-1.2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
 $('.book').click(function(){
 	
 	$('.menu ul li').removeClass("selected");
-	$('.bookFrame').css("display", "block");
+	
 	$('.book').addClass("selected");
+	$('.bookFrame').css("display", "block");
 	$('article').css("display", "none");
+	$('.driveLink').css("display", "none");
 });
 
 
 $('.article').click(function(){
 	
 	$('.menu ul li').removeClass("selected");
+	
+	
+	$('.article').addClass("selected");
 	$('.bookFrame').css("display", "none");
 	$('article').css("display", "block");
+	$('.driveLink').css("display", "none");
+	
+});
+
+$('.drive').click(function(){
+	
+	$('.menu ul li').removeClass("selected");
+	$('.bookFrame').css("display", "none");
+	$('article').css("display", "none");
+	$('.driveLink').css("display", "block");
 	$('.article').addClass("selected");
 	
 });
