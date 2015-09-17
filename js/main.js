ebf3b880b0811a46f55942a5777e0c821fac1af5
Youssef <Youssef.bawakny@gmail.com>
2015-09-17 15:32:53 +0300

    17.09

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 $('.book').click(function(){
 	
-	
+	$('.menu ul li').removeClass("selected");
 	$('.bookFrame').css("display", "block");
+	$('.book').addClass("selected");
+	$('article').css("display", "none");
+});
+
+
+$('.article').click(function(){
+	
+	$('.menu ul li').removeClass("selected");
+	$('.bookFrame').css("display", "none");
+	$('article').css("display", "block");
+	$('.article').addClass("selected");
 	
 });
